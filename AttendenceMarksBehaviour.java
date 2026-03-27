9d175a4d4b1976a34d77c15fd777ef21b9d97ff3
manish-upadhyay12 <upadhyaymanish0654@gmail.com>
2026-03-27 23:43:19 +0530

    Refactor AttendenceMarksBehaviour with comments
    Refactored code for better readability and added comments for clarity.

--- AttendenceMarksBehaviour.java
+++ AttendenceMarksBehaviour.java
@@ -3,15 +3,14 @@ import java.util.*;
 public class AttendenceMarksBehaviour {
     public static void main(String[] args) {
         Scanner  ma = new Scanner(System.in);
-        System.out.print("Enter number of Student :");
-        int n = ma.nextInt();
-        //		System.out.print("Enter subject :");
-        String subject = ma.nextLine();
+        System.out.print("Enter number of Student :");  
+        int n = ma.nextInt();  // take input how many student means length of array
+    
         int arr[]  = new int[n];
 
         for(int i   = 0; i<arr.length; i++) {
             System.out.print("Student "+ (i+1)+ ":" + " Marks = " );
-            arr[i] = ma.nextInt();
+            arr[i] = ma.nextInt();   // Take input
         }
 
         int sum = 0;
@@ -24,24 +23,24 @@ public class AttendenceMarksBehaviour {
             int marks  = arr[i];
             if(arr[i]<=40) {
 
-                System.out.print("Student " + (i+1) +":" + "marks  ="+ arr[i] +" "+": result = fail");
+                System.out.print("Student " + (i+1) +":" + "marks  ="+ arr[i] +" "+": result = fail");  // for fail
                 System.out.println();
             }
             else {
 
-                System.out.print("Student " + (i+1) +":" + "marks  ="+ arr[i]+" " +" "+ ": result = Pass");
-                passCount++;
+                System.out.print("Student " + (i+1) +":" + "marks  ="+ arr[i]+" " +" "+ ": result = Pass"); // for pass
+                passCount++; // if pass then increase
                 System.out.println();
             }
 
 
-            if(marks>Highest) Highest = marks;
-            if(marks<lowest) lowest = marks;
-            sum+= arr[i];
+            if(marks>Highest) Highest = marks;  //  find highest marks
+            if(marks<lowest) lowest = marks;   // find lowest marks 
+            sum+= arr[i];                        // add all marks
         }
-        Average = (double)sum/n;
-        System.out.println("Class summary");
-        System.out.println("Highest marks  = " + Highest);
+        Average = (double)sum/n;     /// find average of marks
+        System.out.println("Class summary");  
+        System.out.println("Highest marks  = " + Highest);   
         System.out.println("lowest marks  = " + lowest);
         System.out.println("Average marks  = " + Average +"%");
         System.out.println("Total pass  = " + passCount);
